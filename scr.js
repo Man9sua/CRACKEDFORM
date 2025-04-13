27f516c1160a11de06d13db37b20ece694799596
Man9sua <161411954+Man9sua@users.noreply.github.com>
2025-04-14 02:15:50 +0500

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,8 @@
+function openModal() {
+	document.getElementById('modalOverlay').style.display = 'block';
+}
+
+function hideModal() {
+	document.getElementById('modalOverlay').style.display = 'none';
+}
+
